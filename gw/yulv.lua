3c70e6b4f7716d9d696fd78348d6fa7674aef23e
chengfangang <chengfangang@qq.com>
2022-01-17 20:03:07 +0800

    add basic handshake process

--- gw/yulv.lua
+++ gw/yulv.lua
@@ -1,7 +1,5 @@
 local require = require
-local lfs   = require("lfs")
-local yaml  = require("tinyyaml")
-local cjson = require("cjson.safe")
+local tcp = ngx.socket.tcp
 
 local tab = require("gw.core.table")
 local protocol     = require("gw.plugins.yulv.protocol")
@@ -20,12 +18,119 @@ _M.name = module_name
 function _M.content_phase()
     ngx.log(ngx.ERR, "ip" .. ngx.var.remote_addr)
     ngx.log(ngx.ERR, "port" .. ngx.var.remote_port)
+
     local key = ngx.var.remote_addr .. ":" ..ngx.var.remote_port
     if connections[key] == nil then
         connections[key] = tab.new(20, 0)
-        connections[key].status = protocol.ClientHandShake
+        connections[key].status = protocol.WaitForServerGreeting
+        local sock, err = tcp()
+        if not sock then
+            return nil, err
+        end
+        connections[key].mysqlsock = sock
+        local port =  3306
+        local host = "127.0.0.1"
+        local ok, err = sock:connect(host, port)
+        if not ok then
+            return nil, "socke connect failed:" .. err
+        end
+
+        local header, data = protocol.get_server_greeting(sock)
+        local reqsock = ngx.req.socket()
+        local bytes, err = reqsock:send(header)
+        if not bytes then
+            return nil, "socket send header failed"
+        end
+        bytes, err = reqsock:send(data)
+        if not bytes then
+            return nil, "socket send data failed"
+        end
+        connections[key].status = protocol.WaitForClientHello
     end
 
+    if connections[key].status == protocol.WaitForClientHello then
+        local reqsock = ngx.req.socket()
+        local header, data = protocol.get_client_hello(reqsock)
+        local mysqlsock = connections[key].mysqlsock
+
+        local bytes, err = mysqlsock:send(header)
+        if not bytes then
+            return nil, "socket send header failed"
+        end
+        bytes, err = mysqlsock:send(data)
+        if not bytes then
+            return nil, "socket send data failed"
+        end
+        connections[key].status = protocol.WaitForServerHello
+
+        header, data = protocol.get_server_hello(mysqlsock)
+        if not header then
+            return nil, "get server hello data failed"
+        end
+        bytes, err = reqsock:send(header)
+        if not bytes then
+            return nil, "socket send header failed"
+        end
+        bytes, err = reqsock:send(data)
+        if not bytes then
+            return nil, "socket send data failed"
+        end
+        connections[key].status = protocol.WaitForAuthSwitchRequest
+    end
+
+    if connections[key].status == protocol.WaitForAuthSwitchRequest then
+        local reqsock = ngx.req.socket()
+        local header, data = protocol.get_client_authrequest_data(reqsock)
+        if not header then
+            return nil, "get header failed"
+        end
+
+        local mysqlsock = connections[key].mysqlsock
+        local bytes, err = mysqlsock:send(header)
+        if not bytes then
+            return nil, "socket send header failed"
+        end
+        bytes, err = mysqlsock:send(data)
+        if not bytes then
+            return nil, "socket send data failed"
+        end
+        connections[key].status = protocol.WaitForAuthSwitchResponse
+
+        header, data = protocol.get_server_authresponse_data(mysqlsock)
+        if not header then
+            return nil, "get header failed"
+        end
+        bytes, err = reqsock:send(header)
+        if not bytes then
+            return nil, "socket send header failed"
+        end
+        bytes, err = reqsock:send(data)
+        if not bytes then
+            return nil, "socket send data failed"
+        end
+
+        connections[key].status = protocol.OK
+    end
+
+    if connections[key].status == protocol.OK then
+        local reqsock = ngx.req.socket()
+        local header, data = protocol.get_client_data(reqsock)
+        if not header then
+            return nil, "get header failed"
+        end
+
+        local mysqlsock = connections[key].mysqlsock
+        local bytes, err = mysqlsock:send(header)
+        if not bytes then
+            return nil, "socket send header failed"
+        end
+        bytes, err = mysqlsock:send(data)
+        if not bytes then
+            return nil, "socket send data failed"
+        end
+
+    end
+    --[[
     local ctx = connections[key]
     if ctx.status == protocol.ClientHandShake then
         ngx.log(ngx.ERR, "HandleShake")
@@ -43,7 +148,8 @@ function _M.content_phase()
     ngx.log(ngx.ERR, "type data:" .. type(data))
 
     ctx.status = protocol.OK
-end
+    --]]
+    end
 
 function _M.log_phase()
     local key = ngx.var.remote_addr .. ":" ..ngx.var.remote_port
--- gw/yulv/protocol.lua
+++ gw/yulv/protocol.lua
@@ -1,8 +1,69 @@
+local strbyte = string.byte
+local bor = bit.bor
+local lshift = bit.lshift
+local rshift = bit.rshift
+
 local _M = {
-    OK = 1,
-    ClientHandShake = 1,
-    ServerHandShake = 2,
-    Header = 4,
+    OK = 0,
+    WaitForServerGreeting = 1,
+    WaitForClientHello = 2,
+    WaitForServerHello = 3,
+    WaitForAuthSwitchRequest = 4,
+    WaitForAuthSwitchResponse = 5,
+
+    HeaderLen = 4,
 }
 
+local function _get_byte3(data, i)
+    local a, b, c = strbyte(data, i, i + 2)
+    return bor(a, lshift(b, 8), lshift(c, 16)), i + 3
+end
+
+local function get_sock_data(sock)
+    local header, err = sock:receive(_M.HeaderLen) -- packet header
+    if not header then
+        return nil, nil, "failed to receive packet header: " .. err
+    end
+    local len, pos = _get_byte3(header, 1)
+    --print("packet length: ", len)
+
+    if len == 0 then
+        return nil, nil, "empty packet"
+    end
+
+    local num = strbyte(header, pos)
+    --print("recv packet: packet no: ", num)
+
+    local packet_no = num
+    local data, err = sock:receive(len)
+    if not data then
+        return nil, nil, "failed to read packet content: " .. err
+    end
+
+    return header, data, nil
+end
+
+function _M.get_server_greeting(sock)
+    return get_sock_data(sock)
+end
+
+function _M.get_client_hello(sock)
+    return get_sock_data(sock)
+end
+
+function _M.get_server_hello(sock)
+    return get_sock_data(sock)
+end
+
+function _M.get_client_authrequest_data(sock)
+    return get_sock_data(sock)
+end
+
+function _M.get_server_authresponse_data(sock)
+    return get_sock_data(sock)
+end
+
+function _M.get_client_data(sock)
+    return get_sock_data(sock)
+end
 return _M
